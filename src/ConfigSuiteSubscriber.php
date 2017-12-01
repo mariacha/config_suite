626548b95266f06b207cb00700c2f01a31ab5911
Maria Fisher <mariacha@gmail.com>
2017-12-01 10:24:15 -0800

    Issue 4 Adding an onConfigDelete listener.

--- src/ConfigSuiteSubscriber.php
+++ src/ConfigSuiteSubscriber.php
@@ -5,11 +5,20 @@ namespace Drupal\config_suite;
 use Drupal\system\SystemConfigSubscriber;
 use Drupal\Core\Config\ConfigImporterEvent;
 use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigEvents;
 
 /**
  * System Config subscriber.
  */
 class ConfigSuiteSubscriber extends SystemConfigSubscriber {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[ConfigEvents::SAVE][] = ['onConfigSave', 255];
+    $events[ConfigEvents::DELETE][] = ['onConfigDelete', 255];
+    return $events;
+  }
 
   /**
    * Ignores the check that the configuration synchronization is from the same site.
@@ -27,7 +36,19 @@ class ConfigSuiteSubscriber extends SystemConfigSubscriber {
   }
 
   public function onConfigSave(ConfigCrudEvent $event) {
+    $this->updateConfig($event, ConfigEvents::SAVE);
+  }
 
+  public function onConfigDelete(ConfigCrudEvent $event) {
+    $this->updateConfig($event, ConfigEvents::DELETE);
+  }
+
+  /**
+   * Almost exactly the same actions should happen on save and delete.
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   * @param string $method
+   */
+  private function updateConfig(ConfigCrudEvent $event, $method) {
     $config = \Drupal::config('config_suite.settings');
     if (!$config->get('automatic_export')) {
       return;
@@ -40,7 +61,12 @@ class ConfigSuiteSubscriber extends SystemConfigSubscriber {
     // Find out which config was saved.
     $config = $event->getConfig();
     $changed_config = $config->getName();
-    $sync_storage->write($changed_config, $active_storage->read($changed_config));
+    if ($method == ConfigEvents::DELETE) {
+      $sync_storage->delete($changed_config);
+    }
+    else {
+      $sync_storage->write($changed_config, $active_storage->read($changed_config));
+    }
 
     // Export configuration collections.
     foreach ($active_storage->getAllCollectionNames() as $collection) {
